49e714cd1276c28c64b27a19d0fc4641fac2cbcd
Matt Cave <m@ttcave.ca>
2021-04-28 11:58:38 -0400

    Add styling with Tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,19 @@ const fs = require("fs");
 const filesize = require("file-size");
 require('dotenv').config();
 
+const now = String(Date.now());
+
 module.exports = (function(eleventyConfig) {
   
-  eleventyConfig.addFilter("filesize", function(path) {
+  eleventyConfig.setUseGitIgnore(false)
+  eleventyConfig.addWatchTarget('./_tmp/style.css')
+  eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
+  
+  eleventyConfig.addShortcode('version', function () {
+    return now
+  });
 
+  eleventyConfig.addFilter("filesize", function(path) {
     let stat = fs.statSync(path);
     if( stat ) {
       return filesize(stat.size).human();
