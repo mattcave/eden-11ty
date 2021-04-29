7041af4a024f707edc1e0216305cf0742d959815
Matt Cave <m@ttcave.ca>
2021-04-28 21:27:28 -0400

    Create basic Location pages

--- _data/contentful.js
+++ _data/contentful.js
@@ -6,16 +6,39 @@ const client = contentful.createClient({
 });
 
 module.exports = function() {
-  return client.getEntries({ content_type: 'jobPosting', order: 'sys.createdAt' })
+
+  let data = {};
+
+  let locationPromise = client.getEntries({ content_type: 'location', order: 'sys.createdAt' })
   .then(function(response) {
-    const data = {};
-    data.jobPostings = response.items
+    const locations = response.items
+    .map(function(location) {
+      location.fields.date= new Date(location.sys.updatedAt);
+      return location.fields;
+    });
+    data.locations = locations;
+    return locations;
+  })
+  .catch(error => console.error(`Error getting data from Contentful ${JSON.stringify(error)}`));
+
+  let jobPostingPromise = client.getEntries({ content_type: 'jobPosting', order: 'sys.createdAt' })
+  .then(function(response) {
+    const jobPostings = response.items
     .map(function(jobPosting) {
-      // page.fields.date= new Date(page.sys.updatedAt);
+      jobPosting.fields.date= new Date(jobPosting.sys.updatedAt);
       jobPosting.fields.jobLocation = jobPosting.fields.jobLocation.fields.name;
       return jobPosting.fields;
     });
-    return data;
-  })    
-  .catch(console.error);
+    data.jobPostings = jobPostings;
+    return jobPostings;
+  })
+  .catch(error => console.error(`Error getting data from Contentful ${JSON.stringify(error)}`));
+
+  return Promise.all([locationPromise, jobPostingPromise])
+  .then(function(result) {
+    return (data)
+  })
+  .catch(error => console.error(`Error getting data from Contentful ${JSON.stringify(error)}`));
 };
+
+
